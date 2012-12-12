d77fce589a6e43aa4ae2e462f68fc60b5eeb691a
Kim Pettersen <kim.a.pettersen@gmail.com>
2012-12-12 08:27:15 -0500

    Added snippets

---
+++ snippets/serializers.py
@@ -0,0 +1,18 @@
+from django.forms import widgets
+from rest_framework import serializers
+from snippets import models
+from django.contrib.auth.models import User
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.Field(source='owner.username')
+    class Meta:
+        model = models.Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.ManyPrimaryKeyRelatedField()
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
---
+++ snippets/views.py
@@ -0,0 +1,34 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
+
+
+
+class SnippetList(generics.ListCreateAPIView):
+    model = Snippet
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    def pre_save(self, obj):
+        obj.owner = self.request.user
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    model = Snippet
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    def pre_save(self, obj):
+        obj.owner = self.request.user
+
+
+class UserList(generics.ListAPIView):
+    model = User
+    serializer_class = UserSerializer
+
+
+class UserInstance(generics.RetrieveAPIView):
+    model = User
+    serializer_class = UserSerializer
